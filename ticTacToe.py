da429b842feb0b6ba40ebe3a4f8ed35b6080d37e
kolton Musgrove <kolton Musgrove>
2017-03-29 14:50:30 -0500

    edited ticTacToe.py

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,6 +1,6 @@
 import random
 
-def gameBorad(place):
+def gameBoard(place):
     print("     |     |     ")#row 1
     print("  "+place[1]+"  |  "+place[2]+"  |  "+place[3]+"  ")#row 2
     print("     |     |     ")#row 3
@@ -15,13 +15,13 @@ def gameBorad(place):
 
 def playerLetter():
     pLetter=""
-    while letter!="X" or letter!="O":
+    while pLetter!="X" or pLetter!="O":
         pLetter=input("Would you like to be X's or O's?")
         pLetter=pLetter.upper()
     if pLetter=="X":
         print("\nOK! You are X's and the computer is O's.")
     else:
-        print("\nOK! You are X's and the computer is O's.")
+        print("\nOK! You are O's and the computer is X's.")
 
 def firstMove():
     start=random.randrange(2)+1
@@ -30,7 +30,17 @@ def firstMove():
     else:
         print("The computer gets to go first.")
 
-def copyBoard(board):
+def copyBoard(place):
     replicateBoard=[]
     for i in board:
-        replicateBoard
+        replicateBoard.append(i)
+    return replicateBoard
+
+def freeSpace(place, turn):
+    return place[turn]==""
+
+def playerTurn(place):
+    turn=""
+    while turn not in "1 2 3 4 5 6 7 8 9".split() or not freeSpace(place, int(turn)):
+        turn=input("Where would you like to go?(1-9)")
+    return int(turn)
