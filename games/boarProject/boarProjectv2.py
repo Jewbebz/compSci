e07dd3935801f3bb0ac6ea9c930c47bb49a9ac42
Micknick9 <19KMusgrove@lcamn.org>
2017-04-16 11:09:11 -0500

    started restructuring

--- games/boarProject/boarProjectv2.py
+++ games/boarProject/boarProjectv2.py
@@ -1,1 +1,28 @@
 #boarProject v2.0
+#------Standard Chance is Calculated as Such:-----
+	#(skillName)*(skillLevel)*(totalLevel)
+	#To change the dificulty we just tru to get anything that is greater than that number out of 'x' arbitrary number
+
+#-----Imports:----
+import random
+import time
+
+#-----Itmes:-----
+items={
+	"Rawhide Armor": "Rough form of protection, stitched together.(+5 protection)",
+	"Leather Satchel": "An old, raggedy sack that carries the few things you have.",
+	"Quiver": "An old quiver you father used, there is a family seal on it's front."
+}
+
+#-----Random Temporary Variables:-----
+
+#-----Mob Info:-----
+boar1Dead=False
+
+#-----Character Info:-----
+name=""
+alive=True
+inventory={""}
+
+for key, value in sorted(items.items()):
+		print(key," = ", value)
