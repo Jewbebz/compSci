92126849749d507d5330dbf787db60558badf8e7
kolton Musgrove <kolton Musgrove>
2017-04-20 08:31:17 -0500

    added a few metta data files, and updated the revized boar project.

---
+++ exampleCode/dictionariesExtended.py
@@ -0,0 +1,6 @@
+dictionary = {"first":1, "second":2, "third":3}
+dictionary2 = dict(first=1, second=2)
+dictionary3 = dict(zip(["first", "Second"], [1, 2]))
+dictionary4 = dict({"first":1, "second":2})
+
+it = iter(dictionary1
