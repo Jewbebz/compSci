a6ca0df777241c0d85abc0b57c04b78449e0ac5b
kolton Musgrove <kolton Musgrove>
2017-04-28 14:37:07 -0500

    Worked on the chatbot, didn't do much

--- projects/chatBot/tensorFlowTest.py
+++ projects/chatBot/tensorFlowTest.py
@@ -2,7 +2,14 @@ import tensorflow as tf
 
 node1 = tf.constant(3.0)
 node2 = tf.constant(4.0)
-node3 = tf.constant(5.2)
 
 sess = tf.Session()
-print(sess.run([node1, node2, node3]))
+print(sess.run([node1, node2]))
+
+node3 = tf.add(node1, node2)
+print("node3: ", node3)
+print("sess.run(node3): ",sess.run(node3))
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b
