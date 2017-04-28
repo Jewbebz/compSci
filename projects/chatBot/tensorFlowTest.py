88f198a0624b876d1f208dd6923c9c9e5dea2a16
kolton Musgrove <kolton Musgrove>
2017-04-28 15:28:20 -0500

    edited the tensorFlowTest.py file

--- projects/chatBot/tensorFlowTest.py
+++ projects/chatBot/tensorFlowTest.py
@@ -13,3 +13,14 @@ print("sess.run(node3): ",sess.run(node3))
 a = tf.placeholder(tf.float32)
 b = tf.placeholder(tf.float32)
 adder_node = a + b
+
+print(sess.run(adder_node, {a: 3, b:4.5}))
+print(sess.run(adder_node, {a: [1,3], b: [2, 4]}))
+
+node1(
+    node1,
+    node1)
+scalar(
+    node2,
+    node2,
+    collection=None)
